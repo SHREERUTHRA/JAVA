e214e00308bd3f561d6052a05125f1a2c0213336
Shreeruthra <110122030@nitt.edu>
2022-11-27 07:36:58 +0000

    Create hello.gitignore and new.js

---
+++ proj.js
@@ -0,0 +1,13 @@
+// npm i to install libraries
+//npm i cowsay
+const cowsay = require("cowsay");
+
+console.log(
+  cowsay.say({
+    text: "Shreeruthra is coding",
+    e: "oO",
+    T: "U ",
+  })
+);
+
+// or cowsay.think()
